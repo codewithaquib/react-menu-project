0801cbe5367dd8de2d861ab7d3ac7619e59b5b25
Aquib Ajani <aquibajani17@gmail.com>
2024-04-02 17:28:43 +0530

    Update Dish.js

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -3,7 +3,7 @@ import React from "react";
 function Dish({ imgURL, title, price, desc }) {
   return (
     <div className="dish" style={{ width: "40%", marginBottom: "50px" }}>
-      <img src={imgURL} alt="Dish Image" className="dishImage" />
+      <img src={imgURL} className="dishImage" />
       <div className="dishData">
         <div className="headingPriceWrapper">
           <h2>{title}</h2>
